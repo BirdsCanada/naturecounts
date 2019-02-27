21275d7376d7b3609ec79964c1766981a9afc33f
Steffi LaZerte <steffi@steffi.ca>
2019-02-27 11:23:43 -0600

    Add species codes data frame

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' List of species codes
+#'
+#' A dataset containing a list of species integer id codes matched to 4-letter
+#' codes according to the Birds Studies Canada authority.
+#'
+#' @format A data frame with 1210 rows and 5 variables:
+#' \describe{
+#'   \item{species_id}{Numeric species id code for use in filtering data}
+#'   \item{species_code}{4-letter species code}
+#' }
+"sp_codes"
--- data-raw/data_creation.R
+++ data-raw/data_creation.R
@@ -7,5 +7,12 @@ max_version <- names(nc_dbs) %>%
   lubridate::as_date() %>%
   max()
 
-
 usethis::use_data(ua, nc_dbs, max_version, internal = TRUE, overwrite = TRUE)
+
+
+# Get species codes
+sp_codes <- srv_query("metadata", "species_codes",
+                      query = list(authority = "BBS")) %>%
+  parse_results(results = FALSE) %>%
+  dplyr::select(species_id, species_code)
+usethis::use_data(sp_codes, internal = FALSE, overwrite = TRUE)
