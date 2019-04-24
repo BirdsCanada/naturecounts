db69a25d1ce2d5ec13418046c03d88748068a4e1
Steffi LaZerte <steffi@steffi.ca>
2019-04-24 14:54:13 -0500

    Remove format file

--- R/format.R
+++
@@ -1,8 +0,0 @@
-
-nc_format <- function(d) {
-
-  suppressMessages(readr::type_convert(d)) %>%
-    dplyr::mutate(date = lubridate::as_date(paste(.data$YearCollected,
-                                                  .data$MonthCollected,
-                                                  .data$DayCollected)))
-}
