6d7d461377b2ab8d4f741fdee5f92742b089331e
Steffi LaZerte <steffi@steffi.ca>
2020-02-25 08:48:20 -0600

    Update internal data and metadata

--- data-raw/data_creation.R
+++ data-raw/data_creation.R
@@ -74,7 +74,7 @@ queries <- dplyr::tribble(
   "site_type",     "siteType",      TRUE)
 
 # Testing Data ------------------------------------------------------------
-test_rc <- nc_data_dl(collections = "RCBIOTABASE", fields_set = "core",
+test_rc <- nc_data_dl(request_id = 152518, fields_set = "core",
                       username = "sample") %>%
   dplyr::filter(CommonName %in% c("Monarch",
                                   "Black Swallowtail",
@@ -89,12 +89,10 @@ usethis::use_data(ua, api, keys, queries, test_rc, internal = TRUE, overwrite = 
 # Get Example Data ------------------------------------------------------------
 
 # Create example databases
-bcch <- nc_data_dl(collections = "RCBIOTABASE", species = 14280,
-                   username = "sample")
+bcch <- nc_data_dl(request_id = 152543, username = "sample")
 usethis::use_data(bcch, internal = FALSE, overwrite = TRUE)
 
-bdow <- nc_data_dl(collections = "RCBIOTABASE", species = 7590,
-                   username = "sample")
+bdow <- nc_data_dl(request_id = 152545, username = "sample")
 usethis::use_data(bdow, internal = FALSE, overwrite = TRUE)
 
 
