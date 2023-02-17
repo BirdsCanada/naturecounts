ccbfed54822ae7b23cebbb655739576069e35d5c
Steffi LaZerte <steffi@steffi.ca>
2023-02-17 15:12:30 -0600

    COSEWIC tools - Double pass grid for iao - checks / tests - 95% EOO

--- R/cosewic_tools.R
+++ R/cosewic_tools.R
@@ -9,6 +9,20 @@
 #' [Distribution](https://cosewic.ca/index.php/en-ca/reports/preparing-status-reports/instructions-preparing-status-reports.html#Distribution)
 #' section in 'Instructions for preparing COSEWIC status reports' 
 #' for more details. 
+#' 
+#' By default the EOO is calculated only using the inner 95% of points (based on
+#' distance to the centroid). This is to ensure that a first-pass of the EOO
+#' does not reject a species from consideration if there are any outlier
+#' observations. However, for a final COSEWIC assessment report, it is likely
+#' better to carefully explore the data to ensure there are no outliers and then
+#' use the full data set (i.e. set `eoo_p = 1`).
+#' 
+#' The IAO is calculated by first assessing large grids (10x large than the
+#' specified size). Only then are smaller grids created within large grid cells
+#' containing observations. This speeds up the process by avoiding the creation
+#' of grids in areas where there are no observations. This means that the plots
+#' and spatial objects may not have grids over large areas lacking observations.
+#' See examples.
 #'
 #' Details on how IAO and EOO are calculated and used
 #' 
@@ -24,18 +38,52 @@
 #' @param coord_lat Character. Name of the column containing latitude. 
 #' @param iao_grid_size_km Numeric. Size of grid (km) to use when calculating
 #'   IAO. Default is COSEWIC requirement (2). Use caution if changing.
-#' @param plots Logical. Whether to return plots
+#' @param eoo_p Numeric. The percentile to calculate the convex hull over.
+#'   Defaults to 0.95 for a 95% convex hull to ensure outlier points do not
+#'   artificially inflate the EOO. Note that for a final COSEWIC report, this
+#'   may not be appropriate. Set to 1 to include all points.
+#' @param plot Logical. Whether to return plot
 #' @param spatial Logical. Whether to return sf spatial objects showing
 #'   calculations.
 #'
-#' @return Data frame or list containing data frame, and (optionally) plots, 
+#' @return Data frame or list containing data frame, and (optionally) plot, 
 #' and spatial data frames.
 #'
 #' @examples
 #' 
+#' # Using the included, test data on black-capped chickadees
+#'
+#' bcch # look at the data
+#'
 #' r <- cosewic_ranges(bcch)
 #' r <- cosewic_ranges(bcch, spatial = FALSE)
-#' r <- cosewic_ranges(bcch, spatial = FALSE, plots = FALSE)
+#' r <- cosewic_ranges(bcch, spatial = FALSE, plot = FALSE)
+#'
+#' # Use multiple species
+#' library(purrr)
+#' library(dplyr)
+#' library(tidyr)
+#' 
+#' # Get multiple species
+#' mult <- rbind(bcch, hofi)
+#' 
+#' # Nest
+#' mult <- nest(mult,  .by = species_id)
+#' mult
+#' 
+#' # Calculate ranges for nested data
+#' r <- mutate(mult, results = map(data, cosewic_ranges, plot = FALSE, spatial = FALSE))
+#' r <- unnest(r, results)
+#' r
+#' 
+#' # To also return plot and spatial
+#' r <- mutate(mult, results = map(data, cosewic_ranges))
+#' r <- unnest_wider(r, results) %>%
+#'   unnest(ranges)
+#' r
+#' 
+#' # Take a look at one specifically
+#' r$plot[[1]]
 #'
 #' @export 
 
@@ -44,65 +92,56 @@ cosewic_ranges <- function(df_db,
                            coord_lon = "longitude",
                            coord_lat = "latitude",
                            iao_grid_size_km = 2,
-                           plots = TRUE,
+                           eoo_p = 0.95,
+                           plot = TRUE,
                            spatial = TRUE) {
   
-  # Check units
-  if(!requireNamespace("sf", quietly = TRUE)) {
-    stop("This function requires the 'sf' package.", 
-         "Please install with `install.packages(\"sf\")` first", call. = FALSE)
-  } else if(packageVersion("sf") < "1.0-9") {
-    stop("This function requires 'sf' version 1.0-9 or higher.",
-         "Please update with `install.packages(\"sf\")` first", call. = FALSE)
-  }
+  # Checks
+  have_sf_check()
+  df <- df_db_check(df_db)
   
-  # Check df_db 
-  # SQLite Connections must become dataframes
-  if(inherits(df_db, "SQLiteConnection")) {
-    
-    if(verbose) message(" - Cannot work directly on SQLite database connections, ",
-                        "collecting data into a data frame...")
-    
-    if(!"naturecounts" %in% DBI::dbListTables(df_db)) {
-      stop("If 'df_db' is a SQLite database, it must have a 'naturecounts' ",
-           "table", call. = FALSE)
-    }
-    
-    df_db <- dplyr::tbl(df_db, "naturecounts") %>%
-      dplyr::collect()
+  # Check species
+  if("species_id" %in% names(df) && dplyr::n_distinct(df$species_id) > 1) {
+    stop("Multiple species detected. See examples in ?cosewic_ranges for how ",
+         "to run multiple species.", call. = FALSE)
   }
   
   # CHECK COORDS
+  if(!all(c(coord_lat, coord_lon) %in% names(df))) {
+    stop("`coord_lat` and `coord_lon` must be columns in `df_db`", call. = FALSE)
+  } else if (!all(is.numeric(df[[coord_lat]]), is.numeric(df[[coord_lat]]))) {
+    stop("`coord_lat` and `coord_lon` must be numeric", call. = FALSE)
+  }
   
   # Note on CRS
   # GeoCAT uses Google's projection EPSG:3857, Web Mercator because of backend
   # Probably should use Canadian projection. Currently using 3347 (Stats 
   # Canada).
   
-  df_sf <- df_db %>%
-    dplyr::select(dplyr::all_of(c(.env$record_id, .env$coord_lon, .env$coord_lat))) %>%
-    sf::st_as_sf(coords = c(coord_lon, coord_lat), crs = 4326)
+  df_sf <- prep_spatial(df)
   
-  # Convert to map units
+  # Set units
   cell_size <- units::as_units(iao_grid_size_km, "km")
-  df_sf_proj <- sf::st_transform(df_sf, 3347)
+
+  # Calculate
+  eoo <- cosewic_eoo(df_sf, p = eoo_p)
+  iao <- cosewic_iao(df_sf, cell_size, record_id)
   
-  iao <- cosewic_iao(df_sf_proj, cell_size, record_id)
-  eoo <- cosewic_eoo(df_sf_proj)
-  
+  # Assemble
   ranges <- dplyr::mutate(iao[["iao"]], eoo = .env$eoo[["eoo"]])
  
   if(ranges$eoo < ranges$iao) {
    message(
    "EOO is less than IAO. This can occur if there are very few, clustered ",
-   "records. Making EOO equal to IAO ",
-   "(see 'Instructions for preparing COSEWIC status reports')")
-    
+   "records.\nMaking EOO equal to IAO ",
+   "(see 'Instructions for preparing COSEWIC status reports'",
+   "\nin ?cosewic_ranges)")
     ranges$eoo <- ranges$iao
   }
   
+  ranges <- dplyr::rename(ranges, !!paste0("eoo_p", eoo_p*100) := "eoo")
   
-  if(plots) {
+  if(plot) {
     a <- dplyr::mutate(iao[["iao_sf"]], 
                        n_records = dplyr::na_if(.data$n_records, 0))
     g <- ggplot2::ggplot() +
@@ -124,8 +163,8 @@ cosewic_ranges <- function(df_db,
     g <- NULL
   } 
   
-  if(plots | spatial) ranges <- list("ranges" = ranges)
-  if(plots) ranges <- append(ranges, list("plots" = g))
+  if(plot | spatial) ranges <- list("ranges" = ranges)
+  if(plot) ranges <- append(ranges, list("plot" = list(g)))
   if(spatial) {
     ranges <- append(
       ranges, 
@@ -137,36 +176,60 @@ cosewic_ranges <- function(df_db,
 }
 
 
-cosewic_iao <- function(df_sf_proj, cell_size, record_id) {
-  grid <- df_sf_proj %>%
-    sf::st_make_grid(cellsize = cell_size) %>%
-    sf::st_as_sf() %>%
-    dplyr::mutate(grid_id = 1:dplyr::n())
+cosewic_iao <- function(df_sf, cell_size, record_id) {
   
+  cell_big <- cell_size * 10
+  
+  grid_lg <- df_sf %>% 
+    sf::st_buffer(cell_size/4) %>%
+    sf::st_make_grid(cellsize = cell_big) %>%
+    sf::st_as_sf() %>%
+    dplyr::mutate(grid_lg_id = 1:dplyr::n())
+  
+  grid <- sf::st_filter(grid_lg, df_sf) %>%
+    split(.$grid_lg_id) %>%
+    purrr::map(~sf::st_make_grid(.x, cellsize = cell_size) %>% 
+                 sf::st_as_sf()) %>%
+    dplyr::bind_rows(.id = "grid_lg_id") %>%
+    dplyr::mutate(grid_id = 1:dplyr::n()) 
+
   iao_sf <- grid %>%
-    sf::st_join(df_sf_proj) %>%
+    sf::st_join(df_sf) %>%
     sf::st_drop_geometry() %>%
-    dplyr::group_by(grid_id) %>%
-    dplyr::mutate(n_records = sum(!is.na(.data[[record_id]]))) %>%
+    dplyr::group_by(.data$grid_lg_id, .data$grid_id) %>%
+    dplyr::summarize(n_records = sum(!is.na(.data[[record_id]])), .groups = "drop") %>%
     dplyr::ungroup()
+  
+  if(sum(iao_sf$n_records) != nrow(df_sf)) {
+    stop("Records incorrectly assigned to grids", call.= FALSE)
+  }
     
   iao <- iao_sf %>%
-    dplyr::summarize(min_record = min(n_records[n_records > 0]),
-                     max_record = max(n_records[n_records > 0]),
-                     median_record = median(n_records[n_records > 0]),
+    dplyr::filter(.data$n_records > 0) %>%
+    dplyr::summarize(min_record = min(n_records),
+                     max_record = max(n_records),
+                     median_record = median(n_records),
                      grid_size_km = .env$cell_size,
-                     n_occupied = sum(n_records != 0), 
+                     n_occupied = dplyr::n(), 
                      iao = n_occupied * .env$cell_size^2)
   
-  iao_sf <- dplyr::right_join(grid, iao_sf, by = "grid_id")
+  iao_sf <- dplyr::right_join(grid, iao_sf, by = c("grid_lg_id", "grid_id"))
   
   list("iao" = iao,
        "iao_sf" = iao_sf)
 }
 
+cosewic_eoo <- function(df_sf, p) {
 
-cosewic_eoo <- function(df_sf_proj) {
-  eoo_sf <- df_sf_proj %>%
+  center <- df_sf %>%
+    dplyr::summarize() %>%
+    sf::st_centroid() %>%
+    sf::st_as_sfc()
+  
+  eoo_sf <- df_sf %>%
+    dplyr::mutate(dist = sf::st_distance(.data$geometry, .env$center)[, 1]) %>%
+    dplyr::filter(dist <= quantile(.data$dist, .env$p)) %>%
+    sf::st_cast(to = "POINT") %>%  
     sf::st_union() %>%
     sf::st_convex_hull() %>%
     sf::st_as_sf() %>%
@@ -177,6 +240,15 @@ cosewic_eoo <- function(df_sf_proj) {
        "eoo_sf" = eoo_sf)
 }
 
+
+prep_spatial <- function(df, 
+                         coords = c("longitude", "latitude"), 
+                         extra = "record_id") {
+  df %>%
+    dplyr::select(dplyr::all_of(c(.env$extra, .env$coords))) %>%
+    sf::st_as_sf(coords = coords, crs = 4326) %>%
+    sf::st_transform(3347)
+}
 
 
 
---
+++ tests/testthat/test_12_cosewic_tools.R
@@ -0,0 +1,67 @@
+test_that("prep_spatial()", {
+  expect_silent(s <- prep_spatial(bcch))
+  expect_s3_class(s, "sf")
+  expect_named(s, c("record_id", "geometry")) 
+  expect_equal(nrow(s), nrow(bcch))
+  expect_equal(format(sf::st_crs(s)), "NAD83 / Statistics Canada Lambert") 
+})
+
+test_that("cosewic_eoo()", {
+  df <- prep_spatial(bcch)
+  expect_silent(e <- cosewic_eoo(df, p = 0.95))
+  expect_type(e, "list")
+  expect_named(e, c("eoo", "eoo_sf"))
+  expect_equal(e[["eoo"]], units::set_units(1758.13, "km2"), tolerance = 0.001)
+  expect_s3_class(e[["eoo_sf"]], "sf")
+  expect_equal(nrow(e[["eoo_sf"]]), 1)
+  expect_equal(as.character(sf::st_geometry_type(e[["eoo_sf"]])), "POLYGON")
+  
+  expect_silent(e <- cosewic_eoo(df, p = 1))
+  expect_equal(e[["eoo"]], units::set_units(4861.251, "km2"), tolerance = 0.001)
+})
+
+
+test_that("cosewic_iao()", {
+  df <- prep_spatial(bcch)
+  expect_silent(a <- cosewic_iao(df, cell_size = units::set_units(2, "km"), record_id = "record_id"))
+  expect_type(a, "list")
+  expect_named(a, c("iao", "iao_sf"))
+  
+  expect_s3_class(a[["iao"]], "data.frame")
+  expect_equal(a[["iao"]], 
+               dplyr::tibble(min_record = 1, max_record = 35, median_record = 1, 
+                             grid_size_km = units::set_units(2, "km"), 
+                             n_occupied = 32, iao = units::set_units(128, "km2")))
+  
+  expect_s3_class(a[["iao_sf"]], "sf")
+  expect_equal(sum(a[["iao_sf"]]$n_records), nrow(bcch))
+  expect_equal(nrow(a[["iao_sf"]]), 1100)
+  expect_equal(unique(as.character(sf::st_geometry_type(a[["iao_sf"]]))),
+               "POLYGON")
+  expect_snapshot_value(a[["iao_sf"]], style = "json2")
+})
+
+
+test_that("cosewic_ranges()", {
+  
+  expect_silent(r <- cosewic_ranges(bcch))
+  expect_type(r, "list")
+  expect_named(r, c("ranges", "plot", "spatial"))
+  expect_equal(
+    r[["ranges"]], 
+    dplyr::tibble(min_record = 1, max_record = 35, median_record = 1, 
+                  grid_size_km = units::set_units(2, "km"), 
+                  n_occupied = 32, iao = units::set_units(128, "km2"),
+                  eoo_p95 = units::set_units(1758.13, "km2")), 
+    tolerance = 0.001)
+              
+  expect_error(cosewic_ranges(rbind(bcch, hofi)), "Multiple species")
+  expect_error(cosewic_ranges(dplyr::select(bcch, -"latitude")),
+               "`coord_lat` and `coord_lon` must be columns in `df_db`")
+  expect_error(cosewic_ranges(dplyr::mutate(bcch, latitude = collection)),
+               "`coord_lat` and `coord_lon` must be numeric")
+  
+  expect_message(r <- cosewic_ranges(bcch[1,]), "EOO is less than IAO")
+  expect_equal(r$ranges$iao, r$ranges$eoo_p95)
+  
+})
