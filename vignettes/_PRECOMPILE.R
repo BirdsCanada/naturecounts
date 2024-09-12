68678c89592df2a6700a0cb60e43f98d4340f93b
dimimarkou <dmarkou@birdscanada.org>
2024-09-12 19:06:42 -0400

    push PRECOMPILE.R

---
+++ vignettes/_PRECOMPILE.R
@@ -0,0 +1,25 @@
+# Pre-compile vignettes which run for a while
+library(knitr)
+library(readr)
+library(stringr)
+
+# Make sure to put figures in local dir in knitr chunk options
+v <- list.files("vignettes", ".orig$", full.names = TRUE, recursive = TRUE)
+
+for(i in v[1:2]) {
+  new <- stringr::str_remove(i, ".orig$")
+  knit(i, new)
+  
+  read_lines(new) %>%
+    str_replace_all("vignettes(/articles)*/", "") %>%
+    write_lines(new)
+}
+
+cache <- list.files("./vignettes", "cache", include.dirs = TRUE,
+                    recursive = TRUE, full.names = TRUE)
+unlink(cache, recursive = TRUE)
+
+#build vignette
+#devtools::build_vignettes()
+#unlink("./doc/", recursive = TRUE)
+#unlink("./Meta/", recursive = TRUE)
